d767686f941f311440be4ee933f49a25eea283ab
tsukudani0412 <koki@tsukudani.work>
2023-03-19 02:16:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Usage
+
+```sh -c "$(curl -L https://raw.githubusercontent.com/tsukudani0412/block-nintendo-for-pihole/main/setup.sh)"```
---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+printf "Adding hosts file to /etc/pihole/gravity.db:\n"
+apt install sqlite3
+sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/tsukudani0412/block-nintendo-for-pihole/main/nintendo-hosts.txt', 1, 'block nintendo servers');"
+printf "https://raw.githubusercontent.com/tsukudani0412/block-nintendo-for-pihole/main/nintendo-hosts.txt\n"
+printf "\n\nAdding custom list to /etc/pihole/custom.list:\n"
+LOCALIP=`ip route get 8.8.8.8 | head -1 | awk '{print $7}'`
+printf "${LOCALIP} conntest.nintendowifi.net\n" | tee -a /etc/pihole/custom.list
+printf "${LOCALIP} ctest.cdn.nintendo.net\n" | tee -a /etc/pihole/custom.list
+
+printf "\nUpdating gravity..."
+systemctl restart pihole-FTL.service > /dev/null
+pihole -g > /dev/null
+printf "done\n"
+
+
+printf "\nSetting up lighttpd server\n"
+mkdir /var/www/html/90dns/ && cd /var/www/html/90dns/
+printf '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><head><title>HTML Page</title></head><body bgcolor="#FFFFFF">This is test.html page</body></html>' | tee conntest.nintendowifi.net.html
+printf 'ok' | tee ctest.cdn.nintendo.net.txt
+
+# modify lighttpd configuration, Pihole recommands to modify settings through external.conf
+cat <<-'EOF' | tee /etc/lighttpd/conf-available/90dns.conf 
+server.modules += (
+        "mod_rewrite"
+        )
+
+# https://redmine.lighttpd.net/boards/2/topics/6541
+var.90dns-response-headers = (
+    "X-Organization" => "Nintendo"
+)
+
+# https://stackoverflow.com/a/32128324/1043209
+$HTTP["host"] == "conntest.nintendowifi.net" {
+    url.rewrite-once = ( ".*" => "/90dns/conntest.nintendowifi.net.html" )
+    setenv.add-response-header = var.90dns-response-headers
+}
+
+$HTTP["host"] == "ctest.cdn.nintendo.net" {
+    url.rewrite-once = ( ".*" => "/90dns/ctest.cdn.nintendo.net.txt" )
+    setenv.add-response-header = var.90dns-response-headers
+    setenv.add-response-header += ( "Content-Type" => "text/plain" )
+}
+EOF
+ln -s /etc/lighttpd/conf-available/90dns.conf /etc/lighttpd/conf-enabled/90dns.conf
+systemctl restart lighttpd.service
+  
